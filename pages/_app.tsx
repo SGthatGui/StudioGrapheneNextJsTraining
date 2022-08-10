7e08f3568b7502f8d95f8bb8729e341287036863
thatGui <guilherme.fonseca@studiographene.com>
2022-08-10 12:50:01 +0100

    initial cleanup + folder structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
+//section for util imports
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+//section for component imports
+
+//section for FC
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
