55bc5b6515698be1553f09194b4a17fd8802d4fe
thatGui <guilherme.fonseca@studiographene.com>
2022-08-11 12:04:03 +0100

    hero component

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+//section for util imports
+import React from 'react'
+import styles from './Hero.module.css'
+
+//section for component imports
+
+//section for FC
+const Hero: React.FC = () => {
+  return <React.Fragment></React.Fragment>
+}
+
+export default Hero
