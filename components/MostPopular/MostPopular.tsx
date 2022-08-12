808ee393580017738a807f5875354aa8df710d4a
thatGui <guilherme.fonseca@studiographene.com>
2022-08-12 14:43:26 +0100

    working on the sorting algo

--- components/MostPopular/MostPopular.tsx
+++ components/MostPopular/MostPopular.tsx
@@ -1,13 +1,35 @@
 //section for util imports
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './MostPopular.module.css'
 import Link from 'next/link'
+import axios from 'axios'
 
 //section for component imports
 import ProductCard from '../ProductCard/ProductCard'
 
 //section for FC
 const ProductsOverview: React.FC = () => {
+  const [motpop, setMotpop] = React.useState()
+
+  const filteredproducts = (arr: any) => {
+    /* arr.filter((el: any) => el.rating.rate > 4.2) */
+    /* console.log(motpop.filter((el: any) => el.rating.rate > 4.2)) */
+    console.log(
+      motpop
+        .sort((a, b) => a.rating.rate - b.rating.rate)
+        .filter((el: any) => el.rating.rate > 4.2)
+    )
+  }
+
+  React.useEffect(() => {
+    axios
+      .get(`https://fakestoreapi.com/products`)
+      .then((response) => {
+        setMotpop(response.data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <React.Fragment>
       <section className={styles.mostpopular}>
@@ -22,6 +44,7 @@ const ProductsOverview: React.FC = () => {
         <div className={styles.prodcat}>
           <ProductCard />
         </div>
+        <button onClick={filteredproducts}>test</button>
       </section>
     </React.Fragment>
   )
